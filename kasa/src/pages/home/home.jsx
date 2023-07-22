11531a2f4de453ab3a8f7e3855eb53f256a01304
CR <celineregnier2003@yahoo.fr>
2023-07-22 15:09:36 +0200

    sam

--- kasa/src/pages/home/home.jsx
+++ kasa/src/pages/home/home.jsx
@@ -42,6 +42,43 @@ function Home() {
 
       </div>
       </main>
+
+{/* récup projet js dynamique */}
+      {/* <div class="mes-projets">
+				<h2>Mes Projets</h2>
+				<div id="modifier3">
+				</div>
+			</div>
+			<div id="filtres">
+			</div>
+			<div id="gallery" class="gallery">
+			</div>
+      // CONSTANTE POUR LA GALERIE DES TRAVAUX
+  const gallery = document.getElementById("gallery");
+ // RECUPERATION DES TRAVAUX
+  const responseWorks = await fetch("http://localhost:5678/api/works");
+  let works = await responseWorks.json();
+  displayWorks(works);
+ //FONCTION POUR RECUPERER LES TRAVAUX
+  function displayWorks(works) {
+    document.getElementById("gallery").innerHTML = "";
+
+    //BOUCLE POUR LA CREATION DES VARIABLES POUR LES TRAVAUX
+    for (let work of works) {
+      let figure = document.createElement("figure");
+      figure.setAttribute("data-categoryid", work.categoryId)
+      let img = document.createElement("img");
+      img.setAttribute("src", work.imageUrl);
+      img.setAttribute("alt", work.title);
+      figure.appendChild(img);
+      let figcaption = document.createElement("figcaption");
+      figcaption.innerHTML = work.title;
+      figure.appendChild(figcaption);
+      gallery.appendChild(figure);
+    }
+  } */}
+
+
     
 
   {/* pour prévisualisation   */}
--- kasa/src/pages/page_apropos/apropos.jsx
+++ kasa/src/pages/page_apropos/apropos.jsx
@@ -10,29 +10,30 @@ function Apropos() {
 
     <div className='valeurs'>
 <ul>
-  <div>
-  <li className='valeur'>Fiabilité ⌃</li>
-  <p className='description_valeur'>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
- régulièrement vérifiées par nos équipes.</p>
-  </div>
-  <div>
-  <li className='valeur'>Respect ⌃</li>
-  <p className='description_valeur'>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-  </div>
-  <div>
-  <li className='valeur'>Service ⌃</li>
-  <p className='description_valeur'>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
-perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-  </div>
-  <div>
-  <li className='valeur'>Sécurité ⌃</li>
-  <p className='description_valeur'>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
+<div>
+<li id="valeur">
+	<p id="description">Ceci est du contenu...</p>
+</li>
+</div>
+  <div><li className='valeur' id ="valeur">Fiabilité <button className='changestyle'>⌃</button></li>
+  <p className='description' id = "description">Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont
+ régulièrement vérifiées par nos équipes.</p></div>
+ <div><li className='valeur'id ="valeur">Respect <button className='changestyle'>⌃</button></li>
+  <p className='description'id = "description">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+perturbation du voisinage entraînera une exclusion de notre plateforme.</p></div>
+<div><li className='valeur'id ="valeur">Service <button className='changestyle'>⌃</button></li>
+  <p className='description'id = "description">La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de
+perturbation du voisinage entraînera une exclusion de notre plateforme.</p></div>
+<div><li className='valeur'id ="valeur">Sécurité <button className='changestyle'>⌃</button></li>
+  <p className='description'id = "description">La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement
 correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au
 locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons
-également des ateliers sur la sécurité domestique pour nos hôtes.</p>
-  </div>
+également des ateliers sur la sécurité domestique pour nos hôtes.</p></div>
+
 </ul>
+
+
+
 
     </div>
 
